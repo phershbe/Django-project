e0c7d48fdd9cf5860ba681fcbe51097e16396334
phershbe <phershbe@gmail.com>
2022-02-19 16:33:43 -0500

    changed url make requirements file

--- theproject/urls.py
+++ theproject/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('thecode/', include('thecode.urls')),
+    path('', include('thecode.urls')),
     path('admin/', admin.site.urls),
 ]
